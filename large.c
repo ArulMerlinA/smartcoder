f65c033b0726d7fface87206754101986c5b4817
ArulMerlinA <30342536+ArulMerlinA@users.noreply.github.com>
2017-08-12 17:01:36 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+{
+int n1,n2,n3;
+printf("enter the three numbers");
+scanf("%d%d%d",&n1,&n2,&n3);
+if((n1>=n2)||(n1>n2))
+{
+print(%d",n1);
+}
+if((n2>=n3)||(n2>n3))
+{
+printf("%d",n2);
+}
+if((n3>=n1)||(n3>n3))
+{
+printf("%d",n3);
+}
+}
